f8399745502cb0cb76e7ae394ed78ad7de647134
t-ha <tjunkha@gmail.com>
2016-11-10 19:40:37 -0800

    fin, swipe right

--- iQuiz/iQuiz/QuestionViewController.swift
+++ iQuiz/iQuiz/QuestionViewController.swift
@@ -17,16 +17,24 @@ class QuestionViewController: UIViewController,UIPickerViewDataSource,UIPickerVi
                                        "Marvel Super Heroes": [["A hammer", "A sword", "An apple", "A shield"], ["Tony Parker", "Peter Parker", "Ben Parker", "Bruce Banner"]],
                                        "Science": [["Earth", "Neptune", "Jupiter", "Mars"], ["Ag", "Gd", "Ga", "Au"]]]
     var isQuestion: Bool = true
+    var finished: Bool = false
     var numCorrect: Int  = 0
     var index: Int  = 0
-    @IBOutlet weak var qView: UIView!
+    var pickedAnswer: String = ""
     @IBOutlet weak var qLabel: UITextView!
     @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var correctAnswerLabel: UILabel!
+    @IBOutlet weak var yourAnswer: UILabel!
     
+    @IBOutlet weak var nextButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerView.delegate = self
         pickerView.dataSource = self
+        answerLabel.isHidden = true
+        correctAnswerLabel.isHidden = true
+        yourAnswer.isHidden = true
         qLabel.text = questions[cat]?[index].0
         // Do any additional setup after loading the view.
     }
@@ -48,17 +56,108 @@ class QuestionViewController: UIViewController,UIPickerViewDataSource,UIPickerVi
         return answers[cat]![index][row]
     }
     
-    @IBAction func tapNext(_ sender: UIButton) {
-        if isQuestion {
-            if index < (questions[cat]?.count)! {
-                self.pickerView.isHidden = true
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickedAnswer = answers[cat]![index][row]
+    }
+    
+    @IBAction func swipeRight(_ sender: Any) {
+        if finished {
+            self.navigationController?.popToRootViewController(animated: true)
+        } else if isQuestion {
+            nextButton.setTitle("Next", for: .normal)
+            self.pickerView.isHidden = true
+            self.answerLabel.isHidden = false
+            self.correctAnswerLabel.isHidden = false
+            self.yourAnswer.isHidden = false
+            let correctAnswer = questions[cat]![index].1
+            self.correctAnswerLabel.text = "Answer: \(correctAnswer)"
+            
+            yourAnswer.text = "Your answer: \(pickedAnswer)"
+            
+            if pickedAnswer == correctAnswer {
+                answerLabel.text = "CORRECT"
+                numCorrect += 1
+            } else {
+                answerLabel.text = "INCORRECT"
             }
-        } else {
-            self.pickerView.isHidden = false
+            pickedAnswer = ""
             index += 1
-            
-            
+        } else if index < (questions[cat]?.count)! {
+            nextButton.setTitle("Submit", for: .normal)
+            self.pickerView.isHidden = false
+            self.answerLabel.isHidden = true
+            self.correctAnswerLabel.isHidden = true
+            self.yourAnswer.isHidden = true
+            qLabel.text = questions[cat]?[index].0
+            pickerView.dataSource = self
+        } else {
+            pickerView.removeFromSuperview()
+            correctAnswerLabel.removeFromSuperview()
+            yourAnswer.removeFromSuperview()
+            let accuracy: Double = Double(numCorrect) / Double(index)
+            if accuracy == 100.0 {
+                answerLabel.text = "Perfect!"
+            } else if accuracy >= 75.0 {
+                answerLabel.text = "Almost!"
+            } else if accuracy >= 40.0 {
+                answerLabel.text = "Try again."
+            } else {
+                answerLabel.text = "Sorry."
+            }
+            qLabel.text = "You got \(numCorrect) out of \(index) correct."
+            finished = true
         }
+        isQuestion = !isQuestion
+    }
+    
+    @IBAction func tapGo(_ sender: UIButton) {
+        if finished {
+            self.navigationController?.popToRootViewController(animated: true)
+        } else if isQuestion {
+            sender.setTitle("Next", for: .normal)
+            self.pickerView.isHidden = true
+            self.answerLabel.isHidden = false
+            self.correctAnswerLabel.isHidden = false
+            self.yourAnswer.isHidden = false
+            let correctAnswer = questions[cat]![index].1
+            self.correctAnswerLabel.text = "Answer: \(correctAnswer)"
+            
+            yourAnswer.text = "Your answer: \(pickedAnswer)"
+            
+            if pickedAnswer == correctAnswer {
+                answerLabel.text = "CORRECT"
+                numCorrect += 1
+            } else {
+                answerLabel.text = "INCORRECT"
+            }
+            pickedAnswer = ""
+            index += 1
+        } else if index < (questions[cat]?.count)! {
+            sender.setTitle("Submit", for: .normal)
+            self.pickerView.isHidden = false
+            self.answerLabel.isHidden = true
+            self.correctAnswerLabel.isHidden = true
+            self.yourAnswer.isHidden = true
+            qLabel.text = questions[cat]?[index].0
+            pickerView.dataSource = self
+        } else {
+            pickerView.removeFromSuperview()
+            correctAnswerLabel.removeFromSuperview()
+            yourAnswer.removeFromSuperview()
+            let accuracy: Double = Double(numCorrect) / Double(index)
+            if accuracy == 100.0 {
+                answerLabel.text = "Perfect!"
+            } else if accuracy >= 75.0 {
+                answerLabel.text = "Almost!"
+            } else if accuracy >= 40.0 {
+                answerLabel.text = "Try again."
+            } else {
+                answerLabel.text = "Sorry."
+            }
+            qLabel.text = "You got \(numCorrect) out of \(index) correct."
+            finished = true
+        }
+        isQuestion = !isQuestion
     }
 
     /*
